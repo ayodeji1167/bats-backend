f573356abd3f6917da49126ed1be0f7208ece486
ayodeji1167 <afuwapeayodeji2018@gmail.com>
2024-03-09 09:01:24 +0100

    added logic to save property files

--- src/property/property.module.ts
+++ src/property/property.module.ts
@@ -3,10 +3,12 @@ import { PropertyService } from './property.service';
 import { PropertyController } from './property.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Property, PropertyModel } from './entities/property.entity';
+import { SharedModule } from 'src/shared/shared.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Property.name, schema: PropertyModel }]),
+    SharedModule,
   ],
   controllers: [PropertyController],
   providers: [PropertyService],
--- src/property/property.service.ts
+++ src/property/property.service.ts
@@ -1,13 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreatePropertyDto } from './dto/create-property.dto';
 import { FilePayload } from './dto/file-upload';
+import { CloudinaryService } from 'src/shared/cloudinary/cloudinary.service';
 
 @Injectable()
 export class PropertyService {
-  create(files: FilePayload, data: CreatePropertyDto) {
+  constructor(private cloudinaryService: CloudinaryService) {}
+  async create(files: FilePayload, data: CreatePropertyDto) {
+    if (!files) {
+      throw new BadRequestException('Please provide the property images');
+    }
+
+    //upload Images
+    console.log('im about to save files');
+    const images = await this.saveFiles(files);
+
+    console.log('the response are ', images);
     return data;
-    // return `This action returns all property`;
   }
+
+  async saveFiles(files: FilePayload) {
+    const res = { mainImage: null, otherImages: [] };
+    if (!files.mainImage || !files.otherImages) {
+      throw new BadRequestException('Please upload the Property Images');
+    }
+    const mainImageRes = await this.cloudinaryService.uploadSingleImage(
+      files?.mainImage,
+      'MAIN_IMAGE'
+    );
+    const otherImagesRes = await this.cloudinaryService.uploadMultipleImages(
+      files?.otherImages,
+      'OTHER_IMAGES'
+    );
+    res.mainImage = mainImageRes;
+    res.otherImages = otherImagesRes;
+    return res;
+  }
+
   findAll() {
     return `This action returns all property`;
   }
